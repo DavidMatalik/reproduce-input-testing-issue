d2699e0702d73c318bbb78712f7c5ceb2055ad2e
David Matalik <david.matalik@posteo.de>
2022-02-23 18:36:11 +0100

    Write test without formik

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [value, setValue] = useState('')
+
+  const doSomething = (e) => {
+    e.preventDefault()
+    setCounter(counter + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form>
+        <label htmlFor='some-input'>Some Input:</label>
+        <input
+          id='some-input'
+          type='text'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type='submit' onClick={doSomething}>
+          Submit
+        </button>
+        <h1>{counter}</h1>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable testing-library/no-debugging-utils */
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from './App'
 
+// Works great
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const button = screen.getByRole('button')
+  const input = screen.getByLabelText('Some Input:')
+  const counter = screen.getByRole('heading')
+
+  userEvent.click(button)
+  expect(counter).toHaveTextContent(1)
+
+  userEvent.type(input, 'Hello')
+
+  expect(input).toHaveValue('Hello')
+})
